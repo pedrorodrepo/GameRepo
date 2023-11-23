eb535bc627e3bcccad7ce7bf66c7df952466e769
Pedro Rodrigues <pedrorodrepo@outlook.com>
2023-11-23 20:01:54 -0300

    WIP

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,21 +3,60 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
+import java.util.*
 
 fun main() {
+    val gson = Gson()
+
+    val reader = Scanner(System.`in`)
+    println("Enter the game ID: ")
+    val search = reader.nextLine()
+
+    val baseAPIUrl = "https://www.cheapshark.com/api/1.0/games?id="
+    val fullUrl = "$baseAPIUrl$search"
+
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create(fullUrl))
         .build()
+
     val response = client
         .send(request, BodyHandlers.ofString())
 
-    val json = response.body()
-    println(json)
+    val jsonBody = response.body()
 
-    val gson = Gson()
-    val myInfoGame = gson.fromJson(json, InfoGame::class.java)
-    val myGame = Game(myInfoGame.info.title, myInfoGame.info.thumb)
+//    try {
+//        val json = response.body()
+//
+//        val myInfoGame = gson.fromJson(json, InfoGame::class.java)
+//        val myGame = Game(myInfoGame.info.title, myInfoGame.info.thumb)
+//
+//        println(myGame.toString())
+//
+//    } catch (ex: Exception) {
+//        println("Game not found, try another ID.")
+//    }
 
-    println(myGame.toString())
+    val result = runCatching {
+        val myInfoGame = gson.fromJson(jsonBody, InfoGame::class.java)
+        val myGame = Game(myInfoGame.info.title, myInfoGame.info.thumb)
+        println(myGame.toString())
+    }
+
+    result.onFailure {
+        println("Game not found, try another ID.")
+    }
+
+    result.onSuccess {
+        println("Do you want to insert a custom description? S/N")
+        val option = reader.nextLine()
+
+        if (option.equals("s", true)) {
+            println("Insert your custom description to the game: ")
+            val description = reader.nextLine()
+        } else {
+            println("The description will be the title of the game")
+
+        }
+    }
 }
